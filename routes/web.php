fc11201a4a6148b3e3f44bc0dd369b813956bdda
ElisDN <mail@elisdn.ru>
2017-12-30 12:13:06 +0300

    Removed welcome page

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index')->name('home');
 
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
