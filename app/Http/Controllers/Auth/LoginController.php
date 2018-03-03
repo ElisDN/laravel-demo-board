dcaed7bc38c0c9654a0f3c20d6f5dc5e157451e2
ElisDN <mail@elisdn.ru>
2018-03-03 23:52:05 +0300

    Inlined auth controllers

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,25 +5,68 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Foundation\Auth\RedirectsUsers;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
-    use AuthenticatesUsers;
-
-    protected $redirectTo = '/cabinet';
+    use ThrottlesLogins;
 
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
     }
 
-    public function authenticated(Request $request, $user)
+    public function showLoginForm()
     {
-        if ($user->status !== User::STATUS_ACTIVE) {
-            $this->guard()->logout();
-            return back()->with('error', 'You need to confirm your account. Please check your email.');
+        return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        if ($this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+            $this->sendLockoutResponse($request);
         }
-        return redirect()->intended($this->redirectPath());
+
+        $authenticate = Auth::attempt(
+            $request->only(['email', 'password']),
+            $request->filled('remember')
+        );
+
+        if ($authenticate) {
+            $request->session()->regenerate();
+            $this->clearLoginAttempts($request);
+            $user = Auth::user();
+            if ($user->status !== User::STATUS_ACTIVE) {
+                Auth::logout();
+                return back()->with('error', 'You need to confirm your account. Please check your email.');
+            }
+            return redirect()->intended(route('cabinet'));
+        }
+
+        $this->incrementLoginAttempts($request);
+
+        throw ValidationException::withMessages(['email' => [trans('auth.failed')]]);
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::guard()->logout();
+        $request->session()->invalidate();
+        return redirect()->route('home');
+    }
+
+    protected function username()
+    {
+        return 'email';
     }
 }
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,21 +5,44 @@ namespace App\Http\Controllers\Auth;
 use App\Mail\Auth\VerifyMail;
 use App\User;
 use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
-use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
 
 class RegisterController extends Controller
 {
-    use RegistersUsers;
-
-    protected $redirectTo = '/cabinet';
-
     public function __construct()
     {
         $this->middleware('guest');
+    }
+
+    public function showRegistrationForm()
+    {
+        return view('auth.register');
+    }
+
+    public function register(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $request['name'],
+            'email' => $request['email'],
+            'password' => bcrypt($request['password']),
+            'verify_token' => Str::random(),
+            'status' => User::STATUS_WAIT,
+        ]);
+
+        Mail::to($user->email)->send(new VerifyMail($user));
+        event(new Registered($user));
+
+        return redirect()->route('login')
+            ->with('success', 'Check your email and click on the link to verify.');
     }
 
     public function verify($token)
@@ -40,37 +63,5 @@ class RegisterController extends Controller
 
         return redirect()->route('login')
             ->with('success', 'Your e-mail is verified. You can now login.');
-    }
-
-    protected function validator(array $data)
-    {
-        return Validator::make($data, [
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6|confirmed',
-        ]);
-    }
-
-    protected function create(array $data)
-    {
-        $user = User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => bcrypt($data['password']),
-            'verify_token' => Str::random(),
-            'status' => User::STATUS_WAIT,
-        ]);
-
-        Mail::to($user->email)->send(new VerifyMail($user));
-
-        return $user;
-    }
-
-    protected function registered(Request $request, $user)
-    {
-        $this->guard()->logout();
-
-        return redirect()->route('login')
-            ->with('success', 'Check your email and click on the link to verify.');
     }
 }
