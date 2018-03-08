0366f20997080fe2ebabf08ed5d593c9aa460946
ElisDN <mail@elisdn.ru>
2018-03-08 14:26:25 +0300

    Added user role console command

---
+++ app/Console/Commands/User/RoleCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands\User;
+
+use App\Entity\User;
+use Illuminate\Console\Command;
+
+class RoleCommand extends Command
+{
+    protected $signature = 'user:role {email} {role}';
+
+    protected $description = 'Set role for user';
+
+    public function handle(): bool
+    {
+        $email = $this->argument('email');
+        $role = $this->argument('role');
+
+        /** @var User $user */
+        if (!$user = User::where('email', $email)->first()) {
+            $this->error('Undefined user with email ' . $email);
+            return false;
+        }
+
+        try {
+            $user->changeRole($role);
+        } catch (\DomainException $e) {
+            $this->error($e->getMessage());
+            return false;
+        }
+
+        $this->info('Role is successfully changed');
+        return true;
+    }
+}
