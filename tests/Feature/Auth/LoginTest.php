a362c9f0f3c9ff6547338a66ed1d5075d8de0d03
ElisDN <mail@elisdn.ru>
2018-03-12 11:16:25 +0300

    Added auth feature tests

---
+++ tests/Feature/Auth/LoginTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use App\Entity\User;
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+    public function testForm(): void
+    {
+        $response = $this->get('/login');
+
+        $response
+            ->assertStatus(200)
+            ->assertSee('Login');
+    }
+
+    public function testErrors(): void
+    {
+        $response = $this->post('/login', [
+            'email' => '',
+            'password' => '',
+        ]);
+
+        $response
+            ->assertStatus(302)
+            ->assertSessionHasErrors(['email', 'password']);
+    }
+
+    public function testWait(): void
+    {
+        $user = factory(User::class)->create(['status' => User::STATUS_WAIT]);
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'secret',
+        ]);
+
+        $response
+            ->assertStatus(302)
+            ->assertRedirect('/')
+            ->assertSessionHas('error', 'You need to confirm your account. Please check your email.');
+    }
+
+    public function testActive(): void
+    {
+        $user = factory(User::class)->create(['status' => User::STATUS_ACTIVE]);
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'secret',
+        ]);
+
+        $response
+            ->assertStatus(302)
+            ->assertRedirect('/cabinet');
+
+        $this->assertAuthenticated();
+    }
+}
---
+++ tests/Feature/Auth/RegisterTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use App\Entity\User;
+use Illuminate\Support\Str;
+use Tests\TestCase;
+
+class RegisterTest extends TestCase
+{
+    public function testForm(): void
+    {
+        $response = $this->get('/register');
+
+        $response
+            ->assertStatus(200)
+            ->assertSee('Register');
+    }
+
+    public function testErrors(): void
+    {
+        $response = $this->post('/register', [
+            'name' => '',
+            'email' => '',
+            'password' => '',
+            'password_confirmation' => '',
+        ]);
+
+        $response
+            ->assertStatus(302)
+            ->assertSessionHasErrors(['name', 'email', 'password']);
+    }
+
+    public function testSuccess(): void
+    {
+        $user = factory(User::class)->make();
+
+        $response = $this->post('/register', [
+            'name' => $user->name,
+            'email' => $user->email,
+            'password' => 'secret',
+            'password_confirmation' => 'secret',
+        ]);
+
+        $response
+            ->assertStatus(302)
+            ->assertRedirect('/login')
+            ->assertSessionHas('success', 'Check your email and click on the link to verify.');
+    }
+
+    public function testVerifyIncorrect(): void
+    {
+        $response = $this->get('/verify/' . Str::uuid());
+
+        $response
+            ->assertStatus(302)
+            ->assertRedirect('/login')
+            ->assertSessionHas('error', 'Sorry your link cannot be identified.');
+    }
+
+    public function testVerify(): void
+    {
+        $user = factory(User::class)->create([
+            'status' => User::STATUS_WAIT,
+            'verify_token' => Str::uuid(),
+        ]);
+
+        $response = $this->get('/verify/' . $user->verify_token);
+
+        $response
+            ->assertStatus(302)
+            ->assertRedirect('/login')
+            ->assertSessionHas('success', 'Your e-mail is verified. You can now login.');
+    }
+}
