a14b5d6a9a6fda9e9bc994df715a4833dd66af83
ElisDN <mail@elisdn.ru>
2018-03-26 15:53:07 +0300

    Extracted user serializer

--- app/Http/Controllers/Api/User/ProfileController.php
+++ app/Http/Controllers/Api/User/ProfileController.php
@@ -5,55 +5,35 @@ namespace App\Http\Controllers\Api\User;
 use App\Entity\User\User;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\ProfileEditRequest;
+use App\Http\Serializers\UserSerializer;
 use App\UseCases\Profile\ProfileService;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
     private $service;
+    /**
+     * @var UserSerializer
+     */
+    private $serializer;
 
-    public function __construct(ProfileService $service)
+    public function __construct(ProfileService $service, UserSerializer $serializer)
     {
         $this->service = $service;
+        $this->serializer = $serializer;
     }
 
     public function show(Request $request): array
     {
-        /** @var User $user */
         $user = $request->user();
-
-        return [
-            'id' => $user->id,
-            'email' => $user->email,
-            'phone' => [
-                'number' => $user->phone,
-                'verified' => $user->phone_verified,
-            ],
-            'name' => [
-                'first' => $user->name,
-                'last' => $user->last_name,
-            ],
-        ];
+        return $this->serializer->profile($user);
     }
 
-    public function update(ProfileEditRequest $request)
+    public function update(ProfileEditRequest $request): array
     {
         $this->service->edit($request->user()->id, $request);
 
-        /** @var User $user */
         $user = User::findOrFail($request->user()->id);
-
-        return [
-            'id' => $user->id,
-            'email' => $user->email,
-            'phone' => [
-                'number' => $user->phone,
-                'verified' => $user->phone_verified,
-            ],
-            'name' => [
-                'first' => $user->name,
-                'last' => $user->last_name,
-            ],
-        ];
+        return $this->serializer->profile($user);
     }
 }
