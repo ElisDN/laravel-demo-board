9f25efb2e8fd65a62c6dc50cd10c679bfbcf1773
ElisDN <mail@elisdn.ru>
2018-03-26 14:28:33 +0300

    Added API register controller

---
+++ app/Http/Controllers/Api/Auth/RegisterController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Http\Controllers\Controller;
+use App\UseCases\Auth\RegisterService;
+use Illuminate\Http\Response;
+
+class RegisterController extends Controller
+{
+    private $service;
+
+    public function __construct(RegisterService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function register(RegisterRequest $request)
+    {
+        $this->service->register($request);
+
+        return response()->json([
+            'success' => 'Check your email and click on the link to verify.'
+        ], Response::HTTP_CREATED);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'Api\HomeController@home');
+Route::post('/register', 'Api\Auth\RegisterController@register');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
